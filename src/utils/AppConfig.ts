ee958a5cf76abf48e68752366273054958817208
jangdu <jjd0324@gmail.com>
2025-02-10 17:56:04 +0900

    docs: update jangdu.blog for site name

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,7 +1,7 @@
 export const AppConfig = {
-  site_name: 'Jangdu Blog',
-  title: 'Jangdu Blog',
-  description: 'Jangdu Blog',
+  site_name: 'blog.jangdu',
+  title: 'blog.jangdu',
+  description: 'blog.jangdu',
   author: 'Jangdu',
   locale_region: 'ko-kr',
   locale: 'ko',
