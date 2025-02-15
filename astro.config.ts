e432cacffd69793517f754cc91503ff68d64c49e
jangdu <jjd0324@gmail.com>
2025-02-15 12:07:47 +0900

    feat: add google analytics config

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,7 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,6 +17,11 @@ export default defineConfig({
     react(),
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
+    }),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
     }),
   ],
   markdown: {
